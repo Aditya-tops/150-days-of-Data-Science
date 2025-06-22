3e70bdf9553c153b4e2cfcbaff064ab5141b009b
Aditya-tops <ap7490944657@gmail.com>
2025-06-22 23:47:43 +0530

    data type

---
+++ 01 Python/L-6/1 variable   .py
@@ -0,0 +1,7 @@
+name = "Aditya"
+age = 16 
+salary = 1000.16
+
+print(name)
+print(age)
+print(salary)
---
+++ 01 Python/L-6/2 prop .py
@@ -0,0 +1,21 @@
+a = 10 
+print(a)
+print(type(a))
+# a  --> Name of the variable
+# 10 --> Value of the variable
+# print(type(a)) --> here type is pre defind function
+# upper()   ---> this is upcoming session
+
+b = 14.5 
+print(b)
+print(type(b))
+# this is a float data type
+
+
+name = "aditya"
+age = 16 
+print(name , age) # commasup  
+print("hi i am " + name + "and my age is " + str(age))
+print("My age is :",age)
+
+# ---> Variable is reuseable 
