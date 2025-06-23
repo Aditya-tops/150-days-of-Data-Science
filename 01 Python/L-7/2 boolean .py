65b101ad2f145ebc5986a61a8cc81ac4baeee2f9
Aditya-tops <ap7490944657@gmail.com>
2025-06-24 01:31:15 +0530

    7

---
+++ 01 Python/L-7/2 boolean .py
@@ -0,0 +1,40 @@
+a = True
+c = "True"
+b = False
+
+print(a)
+print(type(a))
+print(c)
+print(type(c))
+
+
+print(b)
+print(type(b))
+print("----------------------------------")
+
+p = True
+q = False
+
+d = p + p 
+print(d)
+e = q + q
+print(e)
+f = p + q
+print(f)
+
+# true = 1
+# false = 0
+print("========================================")
+
+# impotant question
+
+a = 10
+b = 1.23
+c = None
+print(a)
+print(b)
+print(type(c))
+
+w = None
+w = 10 
+print(w)
---
+++ 01 Python/L-7/3 .py
@@ -0,0 +1,8 @@
+kid_name = None
+kid_name = "kiwi"
+print(kid_name)
+
+
+# None --> keyword to crete None data type
+# print() --> func
+# type() --> func
