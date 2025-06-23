f41a131c3f413d3e3c883bf412f4a0414822122c
Aditya-tops <ap7490944657@gmail.com>
2025-06-23 23:04:21 +0530

    .

---
+++ 01 Python/L-7/1 diff datatype  .py
@@ -0,0 +1,12 @@
+emp_id = 101
+name = "aditya"
+salary = 1000000.36
+
+print("my emp id is : " , emp_id)
+print("my name is : " , name)
+print("my salary is : " , salary)
+print("-------------------------")
+
+print("emp_id data type is : " , type(emp_id))
+print("name data type is : " , type(name))
+print("salary data type is : " , type(salary))
