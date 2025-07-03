f194716fc3cf3cf76288505a5e73472e769cb94e
Aditya-tops <ap7490944657@gmail.com>
2025-07-04 00:45:43 +0530

    Function part-2

---
+++ 01 Python/L-21/0-function part - 2 .py
@@ -0,0 +1,20 @@
+def add(a,b):
+    print(a+b)
+
+add(10,20)
+
+# Note
+# a,b is  formal and Actual arguments
+# 10,20 is values/Actual arguments
+print("---------------------------")
+'''
+    type of argu
+    ------------
+        1.POSITIONAL ARGU
+        2.KEYWORD ARGU
+        3.DEFAULT ARGU
+        4.VARIABLE LENGTH ARGU (*ARGU)
+'''
+
+
+
