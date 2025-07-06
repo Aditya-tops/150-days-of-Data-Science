aa040435d76e468a49ad825d343505f472f6f0ca
Aditya-tops <ap7490944657@gmail.com>
2025-07-06 18:02:15 +0530

    revision

---
+++ 01 Python/L - 27 Revision/1 len() .py
@@ -0,0 +1,32 @@
+a = [10,20,30]
+b = len(a)
+
+'''
+    --> len is predefined function
+    --> We can number of vals in list
+    --> len() func hving return keyword in the background
+
+    return
+    ------
+        1. func without return
+        2. func with return
+'''
+# 1. func without return
+
+def m1():
+    print("hello")
+m1()
+
+print("-------------------------------")
+# 2. func with return
+
+def m2():
+    print("hello")
+    return 1000
+a = m2()
+print(a)
+
+print("----keyword argument----")
+
+
+
