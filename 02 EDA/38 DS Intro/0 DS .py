cabe56c44c80612c337ee48b085c6b5832cf8fe1
Aditya-tops <ap7490944657@gmail.com>
2025-07-11 19:22:17 +0530

    pandas introduction, pandas data structures and installation

---
+++ 02 EDA/38 DS Intro/0 DS .py
@@ -0,0 +1,37 @@
+'''
+    DATA ANALYSIS
+    -------------
+
+        data            -->     collection of facts
+        data            -->     1,2,3,4,5,6,7,8,9,10
+
+        analysis        -->     min val       - 1
+                                max val       - 10
+                                even          - 2,4,6,8,10
+                                odd           - 1,3,5,7,9
+                                total vals    - 10
+                                sum           - 55
+                                a = sum(range(1,11))
+                                print(a)
+
+        list            -->     we can do data analysis
+                        -->     its small scale data
+        
+        DataAnalysis    -->     big scal data
+
+        --> we can do data analysis by using pandas
+        --> pandas is python package
+
+'''
+'''
+    Coin                    =   H               +       T
+
+    Data science Project    =   Data Analysis(70%)   +  model creation(30%)
+                                using pandas         +  using ML
+
+
+
+
+'''
+
+
---
+++ 02 EDA/38 DS Intro/2 pandas DS .py
@@ -0,0 +1,25 @@
+'''
+    pandas          pandas              numpy
+    ------          ------              -----
+
+    List            Series              Array
+    Tuple           DataFrame
+    Set             Panel
+    Dict
+
+    small           big data            big data
+    little slow     very fast           super fast
+
+    pandas:- we are learning about pre-defined class
+    
+
+
+    Data Structure      Dimentiaonality     
+
+    1.Series            1D                  column
+    2.DataFrame         2D                  Rows and Collumns
+    3.Panel             3D                  Group of DataFrame
+
+    pandas -> pandas can handle Structure data
+        
+'''
